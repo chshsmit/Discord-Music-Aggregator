f87745803d6859d1e32a216eaac56990870e1a6e
Christopher Smith <chshsmit@gmail.com>
2020-11-23 14:01:17 -0800

    Trying to print

--- src/start.py
+++ src/start.py
@@ -7,6 +7,8 @@ from DiscordBot import DiscordBot
 load_dotenv()
 DISCORD_GUILD = getenv("DISCORD_GUILD")
 DISCORD_TOKEN = getenv("DISCORD_TOKEN")
+print(DISCORD_GUILD)
+print(DISCORD_TOKEN)
 
 bot_instance = DiscordBot(DISCORD_GUILD, DISCORD_TOKEN)
 
